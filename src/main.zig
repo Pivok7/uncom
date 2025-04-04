b5a7affe8cc335e682ff05d270cd016b58ed3c12
Pivok <pivoc@protonmail.com>
2025-04-04 22:19:52 +0200

    Created repo

---
+++ src/main.zig
@@ -0,0 +1,181 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+const Flags = packed struct{
+    remove: bool = false,
+    force: bool = false,
+    quiet: bool = false,
+};
+
+pub fn main() !void {
+    const stdin = std.io.getStdIn().reader();
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{ .safety = false }){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    var flags: Flags = .{};
+
+    const args = try std.process.argsAlloc(allocator);
+    errdefer std.process.argsFree(allocator, args);
+
+    if (args.len == 1) {
+        printHelp();
+        std.process.exit(0);
+    }
+
+    var input_file_or_null: ?[]const u8 = null;
+
+    for (args[1..]) |arg| {
+        if (std.mem.eql(u8, arg, "-h") or std.mem.eql(u8, arg, "--help")) {
+            printHelp();
+            std.process.exit(0);
+        } else if (std.mem.eql(u8, arg, "-r") or (std.mem.eql(u8, arg, "--remove"))) {
+            flags.remove = true;
+            continue;
+        } else if (std.mem.eql(u8, arg, "-f") or (std.mem.eql(u8, arg, "--force"))) {
+            flags.force = true;
+            continue;
+        } else if (std.mem.eql(u8, arg, "-q") or std.mem.eql(u8, arg, "--quiet")) {
+            flags.quiet = true;
+            continue;
+        } else {
+            input_file_or_null = arg;
+            continue;
+        }
+    }
+
+    const input_file = input_file_or_null orelse {
+        std.debug.print("No input file!\n", .{});
+        std.process.exit(2);
+    };
+
+    if (!try fileExists(input_file)) {
+        std.debug.print("File \"{s}\" does not exist!\n", .{input_file});
+        std.process.exit(1);
+    }
+
+    var command = std.ArrayList([]const u8).init(allocator);
+    defer command.deinit();
+
+    const full_file_path = try std.fs.cwd().realpathAlloc(allocator, input_file);
+    defer allocator.free(full_file_path);
+
+    // Zip
+    if (std.mem.endsWith(u8, input_file, ".zip")) {
+        try command.append("unzip");
+        if (flags.force) try command.append("-o");
+        if (flags.quiet) try command.append("-q");
+        try command.append(full_file_path);
+
+        std.debug.print("Working...\n", .{});
+        try runCommand(allocator, command.items);
+
+        if (flags.remove) {
+            try std.fs.cwd().deleteFile(full_file_path);
+            std.debug.print("Removed: {s}\n", .{input_file});
+        }
+    // 7z
+    } else if (std.mem.endsWith(u8, input_file, ".7z")) {
+        try command.append("7z");
+        try command.append("x");
+        if (flags.force) try command.append("-aoa");
+        if (flags.quiet) try command.append("-bso0");
+        try command.append(full_file_path);
+
+        var output_dir = std.ArrayList(u8).init(allocator);
+        defer output_dir.deinit();
+
+        try output_dir.appendSlice("-o");
+        try output_dir.appendSlice(full_file_path[0..std.mem.lastIndexOf(u8, full_file_path, ".7z").?]);
+
+        try command.append(output_dir.items);
+
+        std.debug.print("Working...\n", .{});
+        try runCommand(allocator, command.items);
+
+        if (flags.remove) {
+            try std.fs.cwd().deleteFile(full_file_path);
+            std.debug.print("Removed: {s}\n", .{input_file});
+        }
+    // Tar
+    } else if (std.mem.endsWith(u8, input_file, ".tar.gz")) {
+        try command.append("tar");
+        try command.append("-xzf");
+        try command.append(full_file_path);
+
+        const output_dir_path = full_file_path[0..std.mem.lastIndexOf(u8, full_file_path, ".tar.gz").?];
+
+        if (flags.force) try std.fs.cwd().deleteTree(output_dir_path);
+
+        if (try fileExists(output_dir_path)) {
+            std.debug.print("Directory already exists. Override? [y/N] ", .{});
+
+            const user_input = (try stdin.readUntilDelimiterOrEofAlloc(allocator, '\n', 1024)).?;
+            defer allocator.free(user_input);
+
+            if (std.mem.eql(u8, user_input, "y") or std.mem.eql(u8, user_input, "Y")) {
+                try std.fs.cwd().deleteTree(output_dir_path);
+                try std.fs.cwd().makeDir(output_dir_path);
+            } else {
+                std.debug.print("Exiting...\n", .{});
+                std.process.exit(0);
+            }
+        } else {
+            try std.fs.cwd().makeDir(output_dir_path);
+        }
+
+        try command.append("-C");
+        try command.append(output_dir_path);
+
+        std.debug.print("Working...\n", .{});
+        try runCommand(allocator, command.items);
+
+        if (flags.remove) {
+            try std.fs.cwd().deleteFile(full_file_path);
+            std.debug.print("Removed: {s}\n", .{input_file});
+        }
+    } else {
+        std.debug.print("File type not supported\n", .{});
+        std.process.exit(1);
+    }
+
+    std.debug.print("Done!\n", .{});
+}
+
+fn runCommand(allocator: Allocator, command: []const []const u8) !void {
+
+    // Initialize the child process
+    var child = std.process.Child.init(command, allocator);
+    _ = try child.spawn(); // Start the process
+
+    // Wait for the process to exit
+    const term = try child.wait();
+
+    // Check exit status
+    switch (term) {
+        .Exited => |code| {
+            if (code != 0) {
+                std.debug.print("Command failed with exit code: {d}\n", .{code});
+            }
+        },
+        else => std.debug.print("Process terminated abnormally\n", .{}),
+    }
+}
+
+fn printHelp() void {
+    std.debug.print("Uncom - universal uncompressor\n", .{});
+    std.debug.print("How to use:\n", .{});
+    std.debug.print("uncom [file_path] [flags]\n", .{});
+    std.debug.print("-r     --remove     Remove archive when finished\n", .{});
+    std.debug.print("-f     --force      Force override output directory\n", .{});
+    std.debug.print("-q     --quiet      Minimize displayed info\n", .{});
+}
+
+fn fileExists(file_path: []const u8) !bool {
+    std.fs.cwd().access(file_path, .{}) catch |err| switch (err) {
+        error.FileNotFound => return false,
+        else => return !false,
+    };
+    return true;
+}
